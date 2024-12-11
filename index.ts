b591f278997b61015ff20195e1aff7263249583c
chiwon.song <chiwon.song@voyagerx.com>
2024-12-11 20:40:26 +0900

    save graph image

---
+++ index.ts
@@ -0,0 +1,4 @@
+import graph from "./graph/MultiAgentGraph";
+import { saveGraph } from "./utils";
+
+await saveGraph("agent_graph.png", graph);
--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
 export function sleep(ms: number) {
@@ -6,4 +8,19 @@ export function sleep(ms: number) {
 
 export function genUUID() {
   return uuidv4();
+}
+
+export async function saveGraph(
+  filename: string,
+  agent: { getGraph: () => any }
+) {
+  const graph = agent.getGraph();
+  const image = await graph.drawMermaidPng();
+  const arrayBuffer = await image.arrayBuffer();
+  const data = new Uint8Array(arrayBuffer);
+
+  const savePath = path.join(__dirname, filename);
+  fs.writeFileSync(savePath, data);
+
+  return savePath;
 }
