52d2120063472e922b91a06af68f2f8688f7312e
chiwon.song <chiwon.song@voyagerx.com>
2024-12-11 20:26:40 +0900

    model use tools

---
+++ index.ts
@@ -0,0 +1,33 @@
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { HumanMessage } from "@langchain/core/messages";
+import { MemorySaver } from "@langchain/langgraph";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { ChatOpenAI } from "@langchain/openai";
+import * as R from "remeda";
+
+// Define the tools for the agent to use
+const agentTools = [new TavilySearchResults({ maxResults: 3 })];
+const agentModel = new ChatOpenAI({ temperature: 0 });
+
+// Initialize memory to persist state between graph runs
+const agentCheckpointer = new MemorySaver();
+const agent = createReactAgent({
+  llm: agentModel,
+  tools: agentTools,
+  checkpointSaver: agentCheckpointer,
+});
+
+// Now it's time to use!
+const agentFinalState = await agent.invoke(
+  { messages: [new HumanMessage("what is the current weather in sf")] },
+  { configurable: { thread_id: "42" } }
+);
+
+console.log(R.last(agentFinalState.messages)!.content);
+
+const agentNextState = await agent.invoke(
+  { messages: [new HumanMessage("what about seoul")] },
+  { configurable: { thread_id: "42" } }
+);
+
+console.log(R.last(agentNextState.messages)!.content);
