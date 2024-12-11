ba1226a385944dc45b57a1690c65ab3870aedee9
chiwon.song <chiwon.song@voyagerx.com>
2024-12-11 20:41:00 +0900

    execute

--- index.ts
+++ index.ts
@@ -1,4 +1,24 @@
 import graph from "./graph/MultiAgentGraph";
-import { saveGraph } from "./utils";
+import { AvengersAnnotationType } from "./state/AvengersState";
+import { genUUID, saveGraph } from "./utils";
 
-await saveGraph("agent_graph.png", graph);
+saveGraph("agent_graph.png", graph);
+
+const initialState: AvengersAnnotationType = {
+  enemy: {
+    name: "Thanos",
+    type: "Monster",
+    hp: 30,
+  },
+  is_defeated: false,
+};
+
+const config = {
+  configurable: {
+    thread_id: genUUID(),
+  },
+  recursionLimit: 40,
+};
+
+const result = await graph.invoke(initialState, config);
+console.log(result);
